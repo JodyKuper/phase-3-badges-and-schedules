ae8913df59be98cf19b85f44e953517a7136d472
Joel Cooper <jodycooper16@gmail.com>
2021-06-09 12:55:26 -0700

    commit tree 3ada9a3f6a45750538af70eab849c7599842d686

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,10 @@
-# Write your code here.
+ def badge_maker(name)
+"Hello, my name is #{name}."
+end
+
+ def batch_badge_creator(speakers)
+    speakers.map do |speaker|
+        "Hello, my name is #{speaker}."
+    end
+end
+
